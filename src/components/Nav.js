d7a5dd2e8579d3ad90c4e78192fc76cca30b0f30
Musa Doci <docimusa@gmail.com>
2022-12-25 16:43:51 +0100

    Create Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+
+
+import React from "react";
+const Header = () => {
+   return (
+      <nav>
+         <img src="./images/logo-airbnb.png" alt=""></img>
+        
+      </nav>
+   );
+};
+export default Header;
