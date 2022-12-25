3c5951bffae5eb8d5d796e4276d901051b305c8f
Musa Doci <docimusa@gmail.com>
2022-12-25 16:41:50 +0100

    Create Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Card(props) {
+   console.log(props);
+   return (
+      <div className="card">
+         <img src={props.img} alt="" className="card-image" />
+         <div className="card-stats">
+            <img src="./images/star.png" alt="" className="card-star" />
+            <span>{props.star}</span>
+            <span className="gray">{props.comment} • </span>
+            <span className="gray">{props.state}</span>
+         </div>
+         <p>{props.paragraf}</p>
+         <p>
+            <span className="bold">{props.price}</span> / person
+         </p>
+      </div>
+   );
+}
